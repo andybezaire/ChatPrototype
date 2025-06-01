94516688bb404cb258d4f40eb5b598cc0eb199ee
andy <andy.bezaire@gmail.com>
2025-06-01 11:11:02 +0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Andy Bezaire on 1.6.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Andy Bezaire on 1.6.2025.
+//
+
+import Testing
+@testable import ChatPrototype
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
